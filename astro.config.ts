4ad956cfab980f2c5bb55fb89c8f996f57db5af0
Roman <dev@rman.dev>
2026-03-22 11:57:11 +0000

    Use `@astrojs/starlight`

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,31 @@
+import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
-  output: "static",
-  vite: { plugins: [tailwindcss()] },
   site: "https://recipes.rman.dev",
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
--- tailwind.config.ts
+++
@@ -1,4 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: { files: ["./src/**/*.{ts,tsx,astro}"] },
-};
